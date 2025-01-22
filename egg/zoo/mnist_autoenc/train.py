d8627727ca21abfd46e5a1083f349f9d0142d040
jankinf <fzw5782@126.com>
2025-01-22 14:34:31 +0800

    script update

--- egg/zoo/mnist_autoenc/train.py
+++ egg/zoo/mnist_autoenc/train.py
@@ -66,13 +66,13 @@ def main(params):
     transform = transforms.ToTensor()
 
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST("./data", train=True, download=True, transform=transform),
+        datasets.MNIST("../data", train=True, download=True, transform=transform),
         batch_size=opts.batch_size,
         shuffle=True,
         **kwargs
     )
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST("./data", train=False, transform=transform),
+        datasets.MNIST("../data", train=False, transform=transform),
         batch_size=opts.batch_size,
         shuffle=True,
         **kwargs
